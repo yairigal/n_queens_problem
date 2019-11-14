bb51d94bb9b6d0c0a7da7e21c350c7d3ca773dda
Yair Yigal <yigalyairn@gmail.com>
2019-11-14 22:57:20 +0200

    added more docs

--- place_and_fix.py
+++ place_and_fix.py
@@ -10,22 +10,32 @@ class QueenMatrix:
 
     Attributes:
         n (number): the amount of queens and the board size.
-        combs (list): all possible combinations for swapping each two queens.
-        steps (number): the amount of queens placed and removed.
+        possible_swaps (list): all possible combinations for swapping each two queens.
+        steps (number): the amount of queens placed and/or removed.
     """
 
     def __init__(self, n):
         self.n = n
         self.steps = 0
-        self.combs = list(itertools.combinations(range(self.n), r=2))
+        self.possible_swaps = list(itertools.combinations(range(self.n), r=2))
 
     @cached_property
     def queens(self):
         """The current state of the queens on the board.
 
-        The state is represented by a list of indices, each value represents the column index of a queen,
-        and each spot index represents the row index of the queen.
-        For example, the list [2,4,3,1,0] encodes the queens -> (0,2) (1,4) (2,3) (3,1) (4,0).
+        The state is represented by a list of indices, for each item in the list, it's index represents the row
+        index of the queen, and it's value represents the column index of the queen.
+        For example, the list [2,4,3,1,0] encodes the queens -> (0,2) (1,4) (2,3) (3,1) (4,0):
+
+        * * Q * *
+        * * * * Q
+        * * * Q *
+        * Q * * *
+        Q * * * *
+
+        To choose the initial state of the queens, the function iterate over each row placing a queen in a spot in
+        the row that is not threatened by other queens. If after some queens placement the next queen has no
+        available spots in its row, then the queen is set to a random spot in that row.
 
         Returns:
             list. the list representation of the queens.
@@ -103,17 +113,15 @@ class QueenMatrix:
     def find_best_swap(self):
         """Find the best swap possible based on heuristic.
 
-        To find which two queens is best to swap, the function calculates the `hit-map`, an array which holds for
-        each spot how much queens threat it.
-        Then, the functions iterates on all possible swaps and checks what are the values which the queens in this
-        swap are going to land on and picks the maximum.
-        The swap with the lowest maximum value is then picked, if there are more then one lowest, pick randomly.
+        Every swap has its cost calculated: the maximum cost value between the spots which the queens will land on if
+        they made the swap. The swap with the lowest maximum value is then picked, if there are more then one lowest,
+        pick randomly.
 
         Returns:
             tuple. the best swap.
         """
         swap_costs = {}
-        for x1, x2 in self.combs:
+        for x1, x2 in self.possible_swaps:
             y1 = self.queens[x1]
             y2 = self.queens[x2]
             swap_costs[x1, x2] = max(self.cost(x1, y2), self.cost(x2, y1))
@@ -123,7 +131,7 @@ class QueenMatrix:
         return x, y
 
     def draw(self, queens_marker='X', empty_spot='*'):
-        """Draw queens matrix board."""
+        """Print the state of the queens on the board."""
         for x, y in itertools.product(range(self.n), repeat=2):
             if y == 0:
                 print()
@@ -142,18 +150,21 @@ class QueenMatrix:
         The algorithm iterates over all two-rows-swap and for each swap maps to the maximum
         landing cost. Then it picks the swap with the lowest maximum cost, and swaps the queens.
 
+        Note:
+            If two swaps are picked over and over, to avoid loops, the function picks the second best swap.
+
         Args:
             verbose (bool): whether to print the board on each iteration.
         """
         last_swap = None
 
         while not self.done:
-
             x1, x2 = self.find_best_swap()
             if last_swap == (x1, x2):  # Avoid loops
-                self.combs.remove(last_swap)
+                # Pick the second best swap
+                self.possible_swaps.remove(last_swap)
                 x1, x2 = self.find_best_swap()
-                self.combs.append(last_swap)
+                self.possible_swaps.append(last_swap)
 
             last_swap = x1, x2
 
--- test_place_and_fix.py
+++ test_place_and_fix.py
@@ -1,6 +1,19 @@
 from place_and_fix import QueenMatrix
 
 
+# HELP FUNCTION
+def create_game_with_queens(new_queens):
+    n = len(new_queens)
+    q = QueenMatrix(n)
+    queens = q.queens
+    for i in range(n):
+        queens[i] = new_queens[i]
+
+    return q
+
+
+# TESTS
+
 def test_is_threat():
     # 0  1  2  3
     # 4  5  6  7
@@ -33,12 +46,71 @@ def test_is_threat():
 
 def test_queen_generation():
     q = QueenMatrix(8)
-    q.queens
+    q.queens  # generate queens
     assert q.steps == 8
     assert sorted(q.queens) == [0, 1, 2, 3, 4, 5, 6, 7]
 
     q = QueenMatrix(4)
-    q.queens
+    q.queens  # generate queens
     assert q.steps == 4
     assert sorted(q.queens) == [0, 1, 2, 3]
 
+
+def test_is_done():
+    # X * * *
+    # * * X *
+    # * X * *
+    # * * * X
+    q = create_game_with_queens([0, 2, 1, 3])
+    assert not q.done
+
+    # * X * *
+    # * * * X
+    # X * * *
+    # * * X *
+    q = create_game_with_queens([1, 3, 0, 2])
+    assert q.done
+
+    winning_sequences = [
+        [3, 6, 2, 7, 1, 4, 0, 5],
+        [4, 1, 3, 6, 2, 7, 5, 0],
+        [3, 1, 6, 2, 5, 7, 4, 0],
+        [3, 5, 7, 2, 0, 6, 4, 1],
+        [2, 5, 7, 0, 3, 6, 4, 1],
+        [4, 2, 7, 3, 6, 0, 5, 1],
+        [4, 6, 3, 0, 2, 7, 5, 1],
+        [3, 0, 4, 7, 5, 2, 6, 1],
+        [2, 5, 3, 0, 7, 4, 6, 1],
+        [5, 1, 6, 0, 3, 7, 4, 2],
+        [3, 6, 0, 7, 4, 1, 5, 2],
+        [5, 3, 6, 0, 7, 1, 4, 2]
+    ]
+
+    losing_sequences = [
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [5, 5, 5, 5, 5, 5, 5, 5],
+        [7, 7, 7, 7, 7, 7, 7, 7],
+        [0, 1, 2, 3, 4, 5, 6, 7],
+        [7, 6, 5, 4, 3, 2, 1, 0]
+    ]
+
+    for state in winning_sequences:
+        q = create_game_with_queens(state)
+        assert q.done
+
+    for state in losing_sequences:
+        q = create_game_with_queens(state)
+        assert not q.done
+
+
+def test_cost():
+    # * X * *
+    # * * * X
+    # X * * *
+    # * * X *
+    q = create_game_with_queens([1, 3, 0, 2])
+    assert q.cost(0, 0) == 2
+    assert q.cost(0, 1) == 1
+    assert q.cost(1, 1) == 3
+    assert q.cost(2, 1) == 3
+    assert q.cost(2, 3) == 4
